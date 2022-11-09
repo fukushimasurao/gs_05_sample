5bca35c1be6034cc782af03caf2ede56ea04cf94
HAYATO FUKUSHIMA <46878156+fukushimasurao@users.noreply.github.com>
2022-11-09 17:12:31 +0900

    presentationとcontrollerの分離

--- index.php
+++ index.php
@@ -1,28 +1,14 @@
 <?php
+
 $connection = new PDO("mysql:host=localhost;dbname=blog_db", 'root', 'root');
 
 $result = $connection->query('SELECT id, title FROM post');
-?>
 
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>List of Posts</title>
-    </head>
-    <body>
-        <h1>List of Posts</h1>
-        <ul>
-            <?php while ($row = $result->fetch(PDO::FETCH_ASSOC)): ?>
-            <li>
-                <a href="show.php?id=<?= $row['id'] ?>">
-                    <?= $row['title'] ?>
-                </a>
-            </li>
-            <?php endwhile ?>
-        </ul>
-    </body>
-</html>
+$posts = [];
+while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+    $posts[] = $row;
+}
 
-<?php
 $connection = null;
-?>
+
+require 'templates/list.php';
