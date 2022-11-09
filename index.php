03353a50dd2050bf139702416bab254ed523defe
HAYATO FUKUSHIMA <46878156+fukushimasurao@users.noreply.github.com>
2022-11-09 19:24:36 +0900

    ルーティング補足追加

--- index.php
+++ index.php
@@ -1,7 +1,21 @@
 <?php
 
 require_once 'model/model.php';
+require_once 'controllers.php';
 
-$posts = get_all_posts();
+// localhostの後の部分を入れる。
+// http://localhost/gs_05_sample だったら、gs_05_sampleを下にいれる。
 
-require 'templates/list.php';
+$url_query = '/gs_05_sample';
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+if ($url_query . '/index.php' === $uri) {
+    list_action();
+} elseif ($url_query . '/index.php/show' === $uri && isset($_GET['id'])) {
+    show_action($_GET['id']);
+} elseif ($url_query . '/index.php/info' === $uri) {
+    show_php_info();
+} else {
+    header('HTTP/1.1 404 Not Found');
+    echo '<html><body><h1>Page Not Found</h1></body></html>';
+}
